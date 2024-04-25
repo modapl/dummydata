b0871ef9a4e620383bc367ef14ee610149d0ecb2
regako <flum0922@naver.com>
2024-04-25 17:45:11 +0900

    Update removeCar.php

--- removeCar.php
+++ removeCar.php
@@ -3,6 +3,22 @@
   "message": "OK",
   "data": {
     "car_list": [
+      {
+        "car_name": "펠리세이드1",
+        "car_number": "111가 1111",
+        "car_image_front": "https://github.com/modapl/dummydata/blob/77dc2766592852bcc6d3e75b53836d3b99158b45/product_img1_square.png?raw=true",
+        "car_image_side": "https://github.com/modapl/dummydata/blob/77dc2766592852bcc6d3e75b53836d3b99158b45/product_img2_square.png?raw=true",
+        "set_current": true,
+        "device_list": []
+      },
+      {
+        "car_name": "펠리세이드3",
+        "car_number": "333가 3333",
+        "car_image_front": "https://github.com/modapl/dummydata/blob/77dc2766592852bcc6d3e75b53836d3b99158b45/product_img3_square.png?raw=true",
+        "car_image_side": "https://github.com/modapl/dummydata/blob/77dc2766592852bcc6d3e75b53836d3b99158b45/product_img4_square.png?raw=true",
+        "set_current": false,
+        "device_list": []
+      }
     ]
   }
 }
